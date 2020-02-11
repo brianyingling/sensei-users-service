4a24d6d5f246402d500be347ff37633c62d1219f
Brian Yingling <brian@jet.com>
2020-02-11 09:37:36 -0500

    print out values

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -13,11 +13,17 @@ COMMIT_SHA1=$CIRCLE_SHA1
 # We must export it so it's available for envsubst
 export COMMIT_SHA1=$COMMIT_SHA1
 
+echo "Running envsubst"
+
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
 envsubst <./manifests/deployment.yaml >./manifests/deployment.yaml.out
 mv ./manifests/deployment.yaml.out ./manifests/deployment.yaml
+
+echo $KUBERNETES_CLUSTER_CERTIFICATE
+echo $KUBERNETES_SERVER
+echo $KUBERNETES_TOKEN
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
