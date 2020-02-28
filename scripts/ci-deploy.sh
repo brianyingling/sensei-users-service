9623dee064f41b21178551d358dbda9097271873
Brian Yingling <brian@jet.com>
2020-02-28 12:55:25 -0500

    envsubst on secrets

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -23,6 +23,10 @@ echo "Running envsubst"
 envsubst <./manifests/deployment.yaml >./manifests/deployment.yaml.out
 mv ./manifests/deployment.yaml.out ./manifests/deployment.yaml
 
+envsubst <./manifests/aws-secrets.yaml >./manifests/aws-secrets.yaml.out
+mv ./manifests/aws-secrets.yaml.out ./manifests/aws-secrets.yaml
+
+
 echo "Printing values"
 echo $KUBERNETES_CLUSTER_CERTIFICATE
 echo $KUBERNETES_SERVER
