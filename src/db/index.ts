df224a37c489999af1ec161b136f70393a6817cf
Brian Yingling <brian@jet.com>
2020-02-28 15:24:18 -0500

    see what is in aws config

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,6 +15,7 @@ const query = (params) => new Promise((resolve, reject) => {
   console.log('making the query...');
   console.log('AWS ACCESS KEY ID in query:', process.env.AWS_ACCESS_KEY_ID);
   console.log('AWS SECRET ACCESS KEY in query:', process.env.AWS_SECRET_ACCESS_KEY);
+  console.log('AWS.config:', AWS.config);
   docClient.query(params, (err, data) => {
     console.log('data:', data);
     if (err) {
